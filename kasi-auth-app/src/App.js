fb5d0ae56b9cd1c60b52a57c1e5b6284026106ac
maggiemoteno2 <motenomaggie@gmail.com>
2020-07-13 15:57:46 +0200

    changes

--- kasi-auth-app/src/App.js
+++ kasi-auth-app/src/App.js
@@ -7,16 +7,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        <h1 className="App-header">
-          Log in page
-        </h1>{" "}
-       <i class="fa fa-user"></i>
-        <div className ="Log-in-form">
-       <input type ="text" placeholder="User Name"/>
-       <br/>
-       <input type ="text" placeholder="password"/>
-       </div>
-       <button>Log in</button>
+     <LogInForm/>
           </div>
       
     );
--- kasi-auth-app/src/container/LogInForm/index.js
+++ kasi-auth-app/src/container/LogInForm/index.js
@@ -3,7 +3,16 @@ import React, { Component } from 'react';
 export default class componentName extends Component {
   render() {
     return (
-      <div> textInComponent </div>
+      <div>  <h1 className="App-header">
+      Log in page
+    </h1>{" "}
+   <i class="fa fa-user"></i>
+    <div className ="Log-in-form">
+   <input type ="text" placeholder="User Name"/>
+   <br/>
+   <input type ="text" placeholder="password"/>
+   </div>
+   <button>Log in</button> </div>
     );
   }
 }
