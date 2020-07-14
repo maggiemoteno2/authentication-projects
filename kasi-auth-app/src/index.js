8588161bcc7e8580badd1618a2431cc8d2a19b7d
maggiemoteno2 <motenomaggie@gmail.com>
2020-07-14 09:47:59 +0200

    added config folder and redux

--- kasi-auth-app/src/index.js
+++ kasi-auth-app/src/index.js
@@ -2,12 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import {store,persistor} from './config/store'
+import { PersistGate } from "redux-persist/integration/react";
 import * as serviceWorker from './serviceWorker';
+import { Provider } from "react-redux";
 
 ReactDOM.render(
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
+  </PersistGate>
+  </Provider>,
   document.getElementById('root')
 );
 
