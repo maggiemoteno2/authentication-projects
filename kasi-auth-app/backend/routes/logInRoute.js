a0e61155bcb789b0d7b47f3850353eb274c8549f
maggiemoteno2 <motenomaggie@gmail.com>
2020-07-14 15:27:49 +0200

    changes

--- kasi-auth-app/backend/routes/logInRoute.js
+++ kasi-auth-app/backend/routes/logInRoute.js
@@ -1,8 +1,18 @@
 const LogInPath = require('./../models/logInModel')
 
 const logIn = function(server){
-    server.post("/loggedIn",(req,res)=>{
-        res.json({ message: "API Working" });
+    server.post("/loggedIn",async(req,res)=>{
+        try {
+            const logInSchema = new LogInPath({
+              firstName: req.body.firstName,
+              password: req.body.password
+            });
+            const logInDetails =logInSchema.save({firstName: req.body.firstName , password:req.body.password});
+            res.status(201).json(logInSchema);
+            console.log(logInDetails)
+          }catch(e){
+        console.log(e)
+          }
 console.log()
     }) 
 
